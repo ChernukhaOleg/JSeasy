735ebd948e42d965e17fdf25e2be33c105f38fad
ChernukhaOleg <cherolserge@gmail.com>
2016-11-25 12:41:45 +0200

    homework №1 Card

---
+++ js/card.js
@@ -0,0 +1,17 @@
+var myCard = {
+    lastName: "Chernukha",
+    firstName: "Oleg",
+    birth: "26.09.1988",
+    devSkills: ["html", "css"],
+};
+var friendCard = {
+    lastName: "Gruscko",
+    firstName: "Mihail",
+    birth: "20.12.1990",
+    devSkils: ["JavaScript", "Ruby", "Python"]
+};
+console.log(myCard);
+console.log(friendCard)
+
+
+       
